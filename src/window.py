0a8f493071d2742404e5fffadcc1820a185ff837
wardtd2 <wardtd@gmail.com>
2024-04-11 17:13:50 -0400

    Made the rest of the Window class variables private and fixed my logic error from renaming running to __running

--- src/window.py
+++ src/window.py
@@ -4,11 +4,11 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
 
-    def __init__(self, height, width):
+    def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
-        self.canvas = Canvas(self.__root,  bg="white", height=height, width=width)
-        self.canvas.pack()
+        self.__canvas = Canvas(self.__root,  bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
     
@@ -21,7 +21,7 @@ class Window:
     
 
     def wait_for_close(self):
-        self.running = True
+        self.__running = True
         while self.__running is True:
             self.redraw()
     
