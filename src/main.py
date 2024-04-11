27f0556220ba8948257c20de298c4312cd025910
wardtd2 <wardtd@gmail.com>
2024-04-11 17:35:05 -0400

    Renamed window to gui_stuff, added Point and Line  classes, tested drawing some lines

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
-from window import Window
+from gui_stuff import Window, Point, Line
 
 
 
 def main():
     win = Window(800, 600)
+    point_1 = Point(100, 0)
+    point_2 = Point(100, 600)
+    point_3 = Point(200, 0)   
+    point_4 = Point(200, 600)
+    point_5 = Point(300, 0)   
+    point_6 = Point(300, 600)
+    win.draw_line(Line(point_1, point_2), "black")
+    win.draw_line(Line(point_3, point_4), "black")
+    win.draw_line(Line(point_5, point_6), "black")
+
     win.wait_for_close()
 
 
