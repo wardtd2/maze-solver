d61edee5d51bbb9487dfd86936468496dbffee66
wardtd2 <wardtd@gmail.com>
2024-04-11 17:03:09 -0400

    Create Window class and use it to create an empty window with a title

---
+++ src/window.py
@@ -0,0 +1,31 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+
+class Window:
+
+    def __init__(self, height, width):
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.canvas = Canvas(self.__root, height=height, width=width)
+        self.canvas.pack()
+        self.running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+    
+
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    
+
+    def wait_for_close(self):
+        self.running = True
+        while self.running is True:
+            self.redraw()
+    
+
+
+    def close(self):
+        self.running = False
