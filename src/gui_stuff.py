c1e1d296f620c235bdf1c1b13a703ce22bba90f9
wardtd2 <wardtd@gmail.com>
2024-04-12 11:41:20 -0400

    Added a cell class to track coordinates of cells and which walls they contain

--- src/gui_stuff.py
+++ src/gui_stuff.py
@@ -13,26 +13,22 @@ class Window:
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
     
 
-
     def redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
 
     
-
     def wait_for_close(self):
         self.__running = True
         while self.__running is True:
             self.redraw()
     
 
-
     def close(self):
         self.__running = False
 
-
     
-    def draw_line(self, line, fill_color):
+    def draw_line(self, line, fill_color="black"):
         line.draw(self.__canvas, fill_color)
 
 
@@ -51,8 +47,36 @@ class Line:
         self.point_1 = point_1
         self.point_2 = point_2
 
-
     
     def draw(self, canvas, fill_color):
         canvas.create_line(self.point_1.x, self.point_1.y, self.point_2.x, self.point_2.y, fill=fill_color, width=2)
         canvas.pack(fill=BOTH, expand=1)
+
+
+
+class Cell:
+    
+    def __init__(
+            self, has_left_wall, has_right_wall,
+                 has_top_wall, has_bottom_wall, x1, y1, x2, y2, window
+                 ):
+        self.has_left_wall = has_left_wall
+        self.has_right_wall = has_right_wall
+        self.has_top_wall = has_top_wall
+        self.has_bottom_wall = has_bottom_wall
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        self._window = window
+
+
+    def draw(self, x1, y1, x2, y2):
+        if self.has_left_wall:
+            self._window.draw_line(Line(Point(x1, y1), Point(x1,y2)))
+        if self.has_right_wall:
+            self._window.draw_line(Line(Point(x2, y1), Point(x2,y2)))
+        if self.has_top_wall:
+            self._window.draw_line(Line(Point(x1, y1), Point(x2,y1)))
+        if self.has_bottom_wall:
+            self._window.draw_line(Line(Point(x1, y2), Point(x2,y2)))
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from gui_stuff import Window, Point, Line
+from gui_stuff import Window, Point, Line, Cell
 
 
 
@@ -10,9 +10,24 @@ def main():
     point_4 = Point(200, 600)
     point_5 = Point(300, 0)   
     point_6 = Point(300, 600)
-    win.draw_line(Line(point_1, point_2), "black")
-    win.draw_line(Line(point_3, point_4), "black")
-    win.draw_line(Line(point_5, point_6), "black")
+    # win.draw_line(Line(point_1, point_2), "black")
+    # win.draw_line(Line(point_3, point_4), "black")
+    # win.draw_line(Line(point_5, point_6), "black")
+
+    cell_0 = Cell(True, True, True, True, 100, 100, 150, 150, win)
+    cell_1 = Cell(True, False, True, True, 200, 100, 250, 150, win)
+    cell_2 = Cell(True, True, False, True, 300, 100, 350, 150, win)
+    cell_3 = Cell(True, True, True, False, 400, 100, 450, 150, win)
+    cell_4 = Cell(False, True, False, True, 500, 100, 550, 150, win)
+    cell_5 = Cell(True, True, True, True, 600, 100, 650, 150, win)
+
+    cell_0.draw(100, 100, 150, 150)
+    cell_1.draw(200, 100, 250, 150)
+    cell_2.draw(300, 100, 350, 150)
+    cell_3.draw(400, 100, 450, 150)
+    cell_4.draw(500, 100, 550, 150)
+    cell_5.draw(600, 100, 650, 150)
+
 
     win.wait_for_close()
 
